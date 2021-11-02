13c6916eb9ff959d53b223191a7a4eafb7ee3304
Jonas-D-M <jonas.de.meyer@student.howest.be>
2021-11-02 11:44:11 +0100

    fix wrong purge paths

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className=" text-red-900 text-center">Hello World!</h1>
+      <h1 className="text-red-900 text-center">Hello World!</h1>
     </div>
   )
 }
