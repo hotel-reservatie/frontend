b78935a5d516cb344d8a83ccb9e68f1a1d67d411
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-11-02 13:55:39 +0100

    graphql helper for queries

---
+++ src/utils/graphQL.ts
@@ -0,0 +1,24 @@
+import { getAuth } from 'firebase/auth'
+const backendurl = ""
+
+export default () => {
+    const query = async (name: string, query: string, variables?: Object) => {
+        const res = await fetch(`${backendurl}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${await getAuth().currentUser?.getIdToken()}`
+            },
+            body: JSON.stringify({
+                query,
+                variables,
+            }),
+        })
+            .then(res => res.json())
+            .catch(error => console.error({ error }))
+        return res.data[name]
+    }
+    return {
+        query
+    }
+}
