0f670da195ab89dcae8b16acea7c06f5f461219b
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-11-05 15:35:31 +0100

    add basic header structure

---
+++ src/components/header/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <p>logo</p>
+      </div>
+      <div>
+        <p>About</p>
+        <p>Rooms</p>
+        <p>Contact</p>
+      </div>
+      <div>
+        <button>Sign In</button>
+        <button>Register</button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
