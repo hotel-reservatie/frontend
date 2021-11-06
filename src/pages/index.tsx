f6bdc92219cd3b82686ca0138bc5901b949a7ce6
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-11-06 15:46:54 +0100

    add grid layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next'
 import Header from 'src/components/header'
 import Card from 'src/components/card'
 import DateInput from 'src/components/input/DateInput'
+import Button from 'src/components/button'
 
 const Home: NextPage = () => {
   const [dates, setDates] = useState<{
@@ -11,37 +12,44 @@ const Home: NextPage = () => {
   }>({ arrival: null, departure: null })
 
   return (
-    <div className="max-w-7xl mx-auto px-6 my-4 md:flex md:flex-row md:align-middle">
-      <div className="mx-auto my-auto mb-8">
-        <h1 className="font-bold text-blue-700 mb-2">Welcome to Hotel MCT!</h1>
-        <p className="text-blue-600 text-left leading-tight">
+    <div className="max-w-7xl mx-auto px-6 my-4 md:grid  md:grid-cols-2 md:mt-16">
+      <div className="mb-8 mx-auto md:my-auto md:px-4">
+        <h1 className="font-bold text-blue-700 mb-2 md:text-4xl">
+          Welcome to Hotel MCT!
+        </h1>
+        <p className="text-blue-600 text-left leading-tight md:text-2xl">
           We provide rooms for every type of vacation goer.
         </p>
-        <p className="text-blue-600 text-left leading-tight">
+        <p className="text-blue-600 text-left leading-tight md:text-2xl">
           Have a look around!
         </p>
       </div>
 
-      <Card>
-        <h1>When would you like to stay with us?</h1>
-        <DateInput
-          placeholder={'Arrival Date'}
-          className="text-center placeholder-blue-500"
-          onChange={(d: Date) => {
-            setDates({ ...dates, arrival: d })
-          }}
-          selected={dates.arrival}
-          value={dates.arrival?.toLocaleDateString()}
-        />
-        <DateInput
-          placeholder={'Departure Date'}
-          className="text-center placeholder-blue-500"
-          onChange={(d: Date) => {
-            setDates({ ...dates, departure: d })
-          }}
-          selected={dates.departure}
-          value={dates.departure?.toLocaleDateString()}
-        />
+      <Card className="w-full md:py-8 md:px-4">
+        <h1 className="text-center mb-4 font-semibold md:text-lg">
+          When would you like to stay with us?
+        </h1>
+        <form action="">
+          <DateInput
+            placeholder={'Arrival Date'}
+            className="text-center placeholder-blue-500"
+            onChange={(d: Date) => {
+              setDates({ ...dates, arrival: d })
+            }}
+            selected={dates.arrival}
+            value={dates.arrival?.toLocaleDateString()}
+          />
+          <DateInput
+            placeholder={'Departure Date'}
+            className="text-center placeholder-blue-500"
+            onChange={(d: Date) => {
+              setDates({ ...dates, departure: d })
+            }}
+            selected={dates.departure}
+            value={dates.departure?.toLocaleDateString()}
+          />
+          <Button>Show availability</Button>
+        </form>
       </Card>
     </div>
   )
