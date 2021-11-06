0eca0cbf2b03fed67ce56287fdaea5b6c8a91ba5
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-11-06 14:46:03 +0100

    add dateinput components on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import type { NextPage } from 'next'
 import Header from 'src/components/header'
 import Card from 'src/components/card'
+import DateInput from 'src/components/input/DateInput'
 
 const Home: NextPage = () => {
+  const [dates, setDates] = useState<{
+    arrival: null | Date
+    departure: null | Date
+  }>({ arrival: null, departure: null })
+
   return (
-    <div className="mx-6 my-4 md:flex md:flex-row md:align-middle">
-      <div className="mx-auto">
+    <div className="max-w-7xl mx-auto px-6 my-4 md:flex md:flex-row md:align-middle">
+      <div className="mx-auto my-auto mb-8">
         <h1 className="font-bold text-blue-700 mb-2">Welcome to Hotel MCT!</h1>
         <p className="text-blue-600 text-left leading-tight">
           We provide rooms for every type of vacation goer.
@@ -18,6 +24,24 @@ const Home: NextPage = () => {
 
       <Card>
         <h1>When would you like to stay with us?</h1>
+        <DateInput
+          placeholder={'Arrival Date'}
+          className="text-center placeholder-blue-500"
+          onChange={(d: Date) => {
+            setDates({ ...dates, arrival: d })
+          }}
+          selected={dates.arrival}
+          value={dates.arrival?.toLocaleDateString()}
+        />
+        <DateInput
+          placeholder={'Departure Date'}
+          className="text-center placeholder-blue-500"
+          onChange={(d: Date) => {
+            setDates({ ...dates, departure: d })
+          }}
+          selected={dates.departure}
+          value={dates.departure?.toLocaleDateString()}
+        />
       </Card>
     </div>
   )
