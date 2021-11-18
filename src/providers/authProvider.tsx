afd9f0b695fe5839f751dfeb18dae0b4e6d4988f
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-11-18 16:13:20 +0100

    convert hook to provider

---
+++ src/providers/authProvider.tsx
@@ -0,0 +1,108 @@
+import {
+  createContext,
+  FunctionComponent,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+import { initializeApp, FirebaseOptions, FirebaseApp } from 'firebase/app'
+import {
+  Auth,
+  browserLocalPersistence,
+  createUserWithEmailAndPassword,
+  getAuth,
+  setPersistence,
+  signInWithEmailAndPassword,
+  signOut,
+  User,
+} from 'firebase/auth'
+
+interface IAuthContext {
+  user: User | null
+  restoreAuth: () => Promise<boolean>
+  createUser: (name: string, email: string, password: string) => void
+  login: (email: string, password: string) => Promise<boolean>
+  logout: () => Promise<void>
+}
+
+const AuthContext = createContext<IAuthContext | null>(null)
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FB_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FB_APIKEY,
+  projectId: process.env.NEXT_PUBLIC_FB_APIKEY,
+  storageBucket: process.env.NEXT_PUBLIC_FB_APIKEY,
+  messagingSenderId: process.env.NEXT_PUBLIC_FB_APIKEY,
+  appId: process.env.NEXT_PUBLIC_FB_APIKEY,
+}
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+export const AuthProvider: FunctionComponent = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null)
+  const auth: Auth = getAuth()
+
+  useEffect(() => {
+    const app: FirebaseApp = initializeApp(firebaseConfig)
+    setPersistence(auth, browserLocalPersistence)
+  }, [])
+
+  const restoreAuth = (): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+      try {
+        auth.onAuthStateChanged(async state => {
+          console.log(state)
+          if (state) {
+            setUser(state)
+            resolve(true)
+          } else {
+            resolve(false)
+          }
+        })
+      } catch (error) {
+        reject
+      }
+    })
+  }
+
+  const createUser = (email: string, password: string) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async userCredential => {
+        setUser(userCredential.user)
+      })
+      .catch(error => {
+        console.log({ error })
+      })
+  }
+
+  const login = (email: string, password: string): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+      signInWithEmailAndPassword(auth, email, password)
+        .then(async userCredential => {
+          setUser(userCredential.user)
+          resolve(true)
+        })
+        .catch(error => {
+          const errorCode = error.code
+          const errorMessage = error.message
+          reject(false)
+        })
+    })
+  }
+
+  const logout = () => {
+    return signOut(auth)
+  }
+
+  const value = {
+    user,
+    restoreAuth,
+    createUser,
+    login,
+    logout,
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
