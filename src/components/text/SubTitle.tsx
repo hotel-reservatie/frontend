1876507e28094fc56bb4bd124b997478c656b41c
Alexander Everaert <alexander.everaert@student.howest.be>
2021-11-20 00:38:44 +0100

    added components for reusability

---
+++ src/components/text/SubTitle.tsx
@@ -0,0 +1,21 @@
+import React, { FunctionComponent } from 'react'
+
+const SubTitle = ({
+  children,
+  className,
+}: {
+  children: any
+  className?: String | undefined
+}) => {
+  return (
+    <h2
+      className={`text-2xl font-semibold text-blue-600 ${
+        className ? className : ''
+      }`}
+    >
+      {children}
+    </h2>
+  )
+}
+
+export default SubTitle
