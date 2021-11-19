8252a975fed1d3486b2fd8e3b38c10bbbe0ad8c8
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-11-19 13:46:58 +0100

    add missing serverside translations

--- src/pages/rooms.tsx
+++ src/pages/rooms.tsx
@@ -1,5 +1,13 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
 const Rooms = () => {
   return <div></div>
 }
+
+export const getStaticProps = async ({ locale }: any) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common'])),
+  },
+})
 
 export default Rooms
