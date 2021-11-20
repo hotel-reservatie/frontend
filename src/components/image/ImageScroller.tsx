34a8229cc96f5a6e0760c73dbb01d13e31f365e4
Alexander Everaert <alexander.everaert@student.howest.be>
2021-11-20 11:00:59 +0100

    moved image scroller in react component

---
+++ src/components/image/ImageScroller.tsx
@@ -0,0 +1,52 @@
+import React, { FormEvent, useEffect, useState } from 'react'
+
+const ImageScroller = ({ images }: { images?: string[] | undefined }) => {
+  const [selectedImage, setSelectedImage] = useState(0)
+
+
+  
+  const handleImageClick = (e: FormEvent) => {
+    const target: HTMLImageElement = e.target as HTMLImageElement
+
+    const id = parseInt(target.id) 
+
+    setSelectedImage(id)
+    
+  }
+
+  useEffect(() => {
+      console.log(images);
+      
+  }, [images])
+  return (
+    <div className="flex flex-col gap-4">
+      <img
+        className="rounded-3xl w-full h-auto max-h-80 object-cover object-center"
+        src={images ? images[selectedImage] : ''}
+        alt=""
+      />
+      <div className="flex justify-center items-center gap-4 min-h-100">
+        {images
+          ? images.map((image: string, index: number) => {
+              return (
+                <button key={image} onClick={handleImageClick}>
+                  <img
+                    src={image}
+                    alt=""
+                    id={index.toString()}
+                    className={`${
+                      index == selectedImage
+                        ? 'w-100 h-100'
+                        : 'w-80 h-80 opacity-50 hover:opacity-75'
+                    } object-cover object-center rounded-2xl transition-all `}
+                  />
+                </button>
+              )
+            })
+          : null}
+      </div>
+    </div>
+  )
+}
+
+export default ImageScroller
--- src/pages/room/[roomId].tsx
+++ src/pages/room/[roomId].tsx
@@ -9,12 +9,11 @@ import Button from 'src/components/button'
 import SubTitle from 'src/components/text/SubTitle'
 import { MdDone, MdOutlinePerson, MdStar } from 'react-icons/md'
 import Card from 'src/components/card'
+import ImageScroller from 'src/components/image/ImageScroller'
 
 const Room: NextPage = () => {
   const router = useRouter()
   const { roomId } = router.query
-
-  const [selectedImage, setSelectedImage] = useState(0)
 
   const { loading, error, data } = useGetRoomByIdQuery({
     variables: { roomId: roomId as string },
@@ -51,44 +50,15 @@ const Room: NextPage = () => {
     return stars
   }
 
-  const handleImageClick = (e: FormEvent) => {
-    const target: HTMLImageElement = e.target as HTMLImageElement
-
-    const id = parseInt(target.id) 
-
-    setSelectedImage(id)
-    
-  }
-
   return (
     <PageLayout>
       <PageTitle>{data?.getRoomById?.roomName}</PageTitle>
       <div className="md:grid md:grid-cols-2 md:mt-16 md:gap-x-16 items-start">
-        <div className="flex flex-col gap-4">
-          <img
-            className="rounded-3xl w-full h-auto"
-            src={data?.getRoomById?.images ? data.getRoomById.images[selectedImage] : ''}
-            alt=""
-          />
-          <div className="flex justify-center items-center gap-4 min-h-128">
-            {data?.getRoomById?.images
-              ? data.getRoomById.images.map((image: string, index: number) => {
-                  return (
-                    <button key={image} onClick={handleImageClick}>
-                      <img
-                        src={image}
-                        alt=""
-                        id={index.toString()}
-                        className={`${
-                          index == selectedImage ? 'w-128 h-128' : 'w-100 h-100 opacity-50 hover:opacity-75'
-                        } object-cover object-center rounded-2xl transition-all `}
-                      />
-                    </button>
-                  )
-                })
-              : null}
-          </div>
-        </div>
+        <ImageScroller
+          images={
+            data?.getRoomById?.images ? data.getRoomById.images : undefined
+          }
+        />
         <div className="grid grid-rows-3 items-center gap-9">
           <h2 className=" text-blue-400 mb">
             <span className=" text-2xl font-bold text-blue-700">{`€${
