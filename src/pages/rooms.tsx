0d74f91df4b1f4260313b2167c41f55dad27a5ae
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-11-20 11:29:00 +0100

    remove range slider

--- src/pages/rooms.tsx
+++ src/pages/rooms.tsx
@@ -1,11 +1,15 @@
+import { useEffect, useState } from 'react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+
+import Slider, { Range } from 'rc-slider'
+import 'rc-slider/assets/index.css'
 import RoomCard from 'src/components/roomCard'
 import { Maybe, RoomFilters, useGetFilteredRoomsQuery } from 'src/schema'
 
 const Rooms = () => {
   const [filters, setFilters] = useState<RoomFilters>()
+  const [boundries, setBoundries] = useState({ min: 0, max: 100 })
   const { query } = useRouter()
   const { loading, error, data } = useGetFilteredRoomsQuery({
     variables: { roomFilter: { ...filters } },
@@ -17,22 +21,37 @@ const Rooms = () => {
     })
   }, [query])
 
+  useEffect(() => {
+    console.log(boundries)
+  }, [boundries])
+
   return (
     <>
-      {data?.getRooms?.map((room, index) => (
-        <RoomCard
-          key={`roomcard-${index}`}
-          type="room"
-          img={room.images?.[0]}
-          title={room.roomName}
-          price={room.currentPrice}
-          size={room.roomType.capacity}
-          surface={room.surface}
-          desc={room.description}
-          id={room.roomId}
-          loading={loading}
-        />
-      ))}
+      <div>
+        <Range min={0} max={boundries.max} step={10} />
+      </div>
+      {data?.getRooms?.map((room, index) => {
+        if (room.currentPrice && boundries.max < room.currentPrice) {
+          setBoundries({ ...boundries, max: room.currentPrice })
+        } else if (room.currentPrice && boundries.min > room.currentPrice) {
+          setBoundries({ ...boundries, min: room.currentPrice })
+        }
+
+        return (
+          <RoomCard
+            key={`roomcard-${index}`}
+            type="room"
+            img={room.images?.[0]}
+            title={room.roomName}
+            price={room.currentPrice}
+            size={room.roomType.capacity}
+            surface={room.surface}
+            desc={room.description}
+            id={room.roomId}
+            loading={loading}
+          />
+        )
+      })}
     </>
   )
 }
