d573b76c63e3f3251dec54dbf19626954d7248c0
Alexander Everaert <alexander.everaert@student.howest.be>
2021-11-23 14:40:36 +0100

    removed unnecessary imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,14 @@
 import type { AppProps } from 'next/app'
-import { ApolloProvider } from '@apollo/client'
 import { appWithTranslation } from 'next-i18next'
-
 import Header from 'src/components/header'
 import '../styles/globals.css'
-import client from 'src/utils/apollo'
-import { AuthProvider, useAuth } from 'src/providers/authProvider'
-import { useEffect } from 'react'
+import { AuthProvider } from 'src/providers/authProvider'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-        <Header />
-        <Component {...pageProps} />
+      <Header />
+      <Component {...pageProps} />
     </AuthProvider>
   )
 }
