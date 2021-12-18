e21d9bd5138c37aef228981bb1b47af69bdea6a7
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-12-18 14:46:38 +0100

    add translate component

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,16 +2,17 @@ import PageLayout from 'src/components/layout/PageLayout'
 import PageTitle from 'src/components/text/PageTitle'
 import Image from 'next/image'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { useTranslation } from 'next-i18next'
+import Translater from 'src/components/translater'
 
 const About = () => {
-  const { t } = useTranslation('common')
   return (
     <PageLayout className="flex flex-col h-full">
       <PageTitle>Hotel MCT</PageTitle>
       <div className="grid grid-cols-2 place-items-center gap-x-4">
         <div>
-          <p>{t('about.text')}</p>
+          <p>
+            <Translater>{'about.text'}</Translater>
+          </p>
         </div>
         <div className="relative h-full w-full">
           <Image
